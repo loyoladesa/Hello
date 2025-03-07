7c5fae81b25406fbaaa008f8bf24a107f8fd136c
loyol <loyoladesa@gmail.com>
2025-03-07 15:26:26 -0300

    if else

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+
+const getFullName = (
+  firstName: string,
+  secondName: string,
+  thirdName: string,
+) => {
+
+  if (thirdName == '') {
+    return firstName + ' ' + secondName + ' ' + thirdName;
+  }
+  else{
+    return firstName;
+  }
+  
+};
+
+
 const Cat = () => {
   const name = "Sidney"
-  return <Text>Hello, I am {name}!</Text>;
+  return <Text>Hello, I am {getFullName('Sidney','Loyola','')}!</Text>;
 };
 
 export default Cat;
